8fe6725488fb5986c139604b962502d430a7c436
v01d <lolofon@icloud.com>
2019-02-26 22:02:46 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const execSync = require('child_process').execSync
+
+const path = require('path')
+const app = express()
+const port = 8081
+
+app
+.get('/', (req, res) => {
+    res.sendFile(path.resolve('./index.html'))
+})
+.get('/shutdown', (req, res) => {
+    res.send('Sending shutdown command ..........')
+    code = execSync('shutdown /s /t 10');
+    console.log(code);
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`)) 
